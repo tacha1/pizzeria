262807215dfe41daecdbb76f5aa85eb8439f0a86
tacha1 <uwarosine10@gmail.com>
2020-10-10 20:34:21 +0200

    creating a javascript file

--- script.js
+++ script.js
@@ -20,3 +20,57 @@ function validate(){
     c=a+b;
     document.getElementById('res').value=c+'Rwf';
 }
+$(".paragraph1").hide();
+
+    $(".photo1").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph1").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph1").fadeOut();
+    });
+
+    $(".paragraph2").hide();
+    $(".photo2").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph2").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph2").fadeOut();
+    });
+
+    $(".paragraph3").hide();
+    $(".photo3").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph3").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph3").fadeOut();
+    });
+
+    $(".paragraph4").hide();
+    $(".photo4").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph4").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph4").fadeOut();
+    });
+
+    $(".paragraph5").hide();
+    $(".photo5").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph5").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph5").fadeOut();
+    });
+
+    $(".paragraph6").hide();
+    $(".photo6").hover(function(){
+        $(this).stop().animate({opacity:.2},200);
+        $(".paragraph6").fadeIn();
+    },function(){
+        $(this).stop().animate({opacity:2},500);
+        $(".paragraph6").fadeOut();
+    });
