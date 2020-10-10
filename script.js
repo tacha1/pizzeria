4f7baf8be7b42623a729eedebdc27133f209f0ba
tacha1 <uwarosine10@gmail.com>
2020-10-10 21:58:38 +0200

    CREATING CSS FILE

--- script.js
+++ script.js
@@ -1,76 +1,75 @@
-function validate(){
-    var chks=document.getElementsByName('chk[]');
-    var hasChecked=false;
-    for(var i=0;i<chks.length;i++)
-        {
-            if(chks[i].checked)
-                {
-                    hasChecked=true;
-                    break;
-                }
+function validate() {
+    var chks = document.getElementsByName('chk[]');
+    var hasChecked = false;
+    for (var i = 0; i < chks.length; i++) {
+        if (chks[i].checked) {
+            hasChecked = true;
+            break;
         }
-    if(hasChecked==false)
-        {
-            alert("Select any item");
-            return false;
-        }
-    var a,b,c;
-    a=Number(document.getElementById('sec').value);
-    b=Number(document.getElementById('select').value);
-    c=a+b;
-    document.getElementById('res').value=c+'Rwf';
+    }
+    if (hasChecked == false) {
+        alert("Select any item");
+        return false;
+    }
+    var a, b, c;
+    a = Number(document.getElementById('sec').value);
+    b = Number(document.getElementById('select').value);
+    c = a + b;
+    document.getElementById('res').value = c + 'Rwf';
 }
-$(".paragraph1").hide();
+$(document).ready(function () {
+    $(".paragraph1").hide();
 
-    $(".photo1").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo1").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph1").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph1").fadeOut();
     });
 
     $(".paragraph2").hide();
-    $(".photo2").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo2").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph2").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph2").fadeOut();
     });
 
     $(".paragraph3").hide();
-    $(".photo3").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo3").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph3").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph3").fadeOut();
     });
 
     $(".paragraph4").hide();
-    $(".photo4").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo4").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph4").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph4").fadeOut();
     });
 
     $(".paragraph5").hide();
-    $(".photo5").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo5").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph5").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph5").fadeOut();
     });
 
     $(".paragraph6").hide();
-    $(".photo6").hover(function(){
-        $(this).stop().animate({opacity:.2},200);
+    $(".photo6").hover(function () {
+        $(this).stop().animate({ opacity: .2 }, 200);
         $(".paragraph6").fadeIn();
-    },function(){
-        $(this).stop().animate({opacity:2},500);
+    }, function () {
+        $(this).stop().animate({ opacity: 2 }, 500);
         $(".paragraph6").fadeOut();
     });
+});
